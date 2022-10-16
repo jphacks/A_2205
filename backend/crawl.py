42d3dcba83dcb9a609829eaf5ab1bb83ca31fd9b
Kosuke Takahashi <61932642+31159piko-suke@users.noreply.github.com>
2022-10-16 18:23:38 +0900

    modified to save auther_name to csv

--- backend/crawl.py
+++ backend/crawl.py
@@ -9,7 +9,9 @@ def crawl_tweets(username: str, max_results: int = 10):
     df_tweets = pd.read_csv(f"users/{username}/tweets.csv")
     user = api.get_user(username=username).data
     user_id = user.get("id")
-    liked_tweets = api.get_liked_tweets(id=user_id, max_results=max_results).data
-    for tweet in liked_tweets:
-        df_tweets = df_tweets.append({"id": tweet.id, "text": tweet.text}, ignore_index=True)
+    tweet_info = api.get_liked_tweets(id=user_id, max_results=max_results, expansions="author_id")
+    author_info = tweet_info.includes['users']
+    liked_tweets = tweet_info.data
+    for author, tweet in zip(author_info, liked_tweets):
+        df_tweets = df_tweets.append({"id": tweet.id, "text": tweet.text, "author_name": author.username}, ignore_index=True)
     df_tweets.to_csv(f"users/{username}/tweets.csv", index=False)
