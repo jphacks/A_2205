d85be9b2ffdc26233bb6f4a3d08ea65194643bb7
Kosuke Takahashi <61932642+31159piko-suke@users.noreply.github.com>
2022-10-16 18:24:20 +0900

    modified to save auther_name to csv

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,34 @@
 import streamlit as st
+import streamlit.components.v1 as components 
+import requests
+import json
+import os
 
-st.write("Hello, world!")
+def tweet_to_html(url):
+    api = f"https://publish.twitter.com/oembed?url={url}"
+    res = requests.get(api)
+    response = res.json()
+    return response["html"]
+
+def tweet_list():
+    # username = st.session_state.twitter_id
+    username = "i_amppp"
+    if username:
+        res = requests.post(f"http://api_server:8080/create_user/{username}")
+        st.write(res)
+        res = requests.post(f"http://api_server:8080/update/{username}")
+        st.write(res)
+
+        if st.button("ツイートを表示する"):
+            res = requests.get(f'http://api_server:8080/tweets/{username}')
+
+            result = res.json()["data"]
+            tweet_id = json.loads(result)["id"]
+            author_name = json.loads(result)["author_name"]
+
+            for id, name in zip(tweet_id.values(), author_name):
+                url = f"https://twitter.com/{name}/status/{id}"
+                html = tweet_to_html(url)
+                st.components.v1.html(html)
+
+tweet_list()
