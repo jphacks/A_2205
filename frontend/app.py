bf3dc3ecf2d2f0548208af8eba35415af39c74ed
YusukeTakahashi <yusuke@172.16.36.208.ige.tohoku.ac.jp>
2022-10-17 18:47:10 +0900

    Added choose-topic-page

--- frontend/app.py
+++ frontend/app.py
@@ -1,34 +1,52 @@
+import base64
 import streamlit as st
 import streamlit.components.v1 as components
 import requests
 import json
+import copy
 import os
 
-def tweet_to_html(url):
-    api = f"https://publish.twitter.com/oembed?url={url}"
-    res = requests.get(api)
-    response = res.json()
-    return response["html"]
+from login import login
+from choose_topic import choose_topic
+from tweet_list import tweet_list
+from utils import set_png_as_page_bg
 
-def tweet_list():
-    # username = st.session_state.twitter_id
-    username = "i_amppp"
-    if username:
-        res = requests.post(f"http://api_server:8080/user/{username}")
-        st.write(res)
-        res = requests.post(f"http://api_server:8080/update/{username}")
-        st.write(res)
 
-        if st.button("ツイートを表示する"):
-            res = requests.get(f'http://api_server:8080/tweets/{username}')
+BACKGROUND_IMAGE_PATH = {
+    'login': 'images/login.png',
+    'choose_topic': 'images/black.png',
+    'tweet_list': 'images/black.png',
+    'page2': 'images/black.png'
+}
 
-            result = json.loads(res.json()["data"])
-            for tweet in result:
-                tweet_id = tweet["id"]
-                author_name = tweet["author_name"]
+def init():
+    if st.session_state.get('init') is None:
+        st.session_state.init=True
+        st.session_state.page_name = 'login'
+        st.session_state.twitter_id = None
+        st.session_state.option_topic = [
+            'ファッション','ビューティー', 'アウトドア', 'アート', 'カルチャー',
+            'アニメ', '漫画', 'ビジネス', '金融', '食べ物', '旅行', 'エンターテインメント',
+            '音楽', 'ゲーム業界', 'キャリア', 'ライフスタイル', 'フィットネス',
+            'スポーツ', 'テクノロジー', '科学'
+        ]
+        st.session_state.add_topic = False
+        st.session_state.default_topic = []
 
-                url = f"https://twitter.com/{author_name}/status/{tweet_id}"
-                html = tweet_to_html(url)
-                st.components.v1.html(html)
+def page2():
+    with st.container():
+        st.write('page2')
 
-tweet_list()
+pages = {'login':login, 'choose_topic':choose_topic, 'tweet_list':tweet_list, 'page2':page2}
+
+
+init()
+
+# set background
+bg_img_path = BACKGROUND_IMAGE_PATH[st.session_state.page_name]
+set_png_as_page_bg(bg_img_path)
+
+now_page = pages[st.session_state.page_name]
+now_page()
+
+
--- frontend/choose_topic.py
+++ frontend/choose_topic.py
@@ -1,41 +1,51 @@
+import copy
+import time
+
 import streamlit as st
 
-def choose_topic_page():
-    from audioop import add
-    from click import option
-    from numpy import choose
-    import streamlit as st
 
-    option_topic = [
-        'ファッション','ビューティー', 'アウトドア', 'アート', 'カルチャー',
-        'アニメ', '漫画', 'ビジネス', '金融', '食べ物', '旅行', 'エンターテインメント',
-        '音楽', 'ゲーム業界', 'キャリア', 'ライフスタイル', 'フィットネス',
-        'スポーツ', 'テクノロジー', '科学'
-    ]
+def choose_topic():
+    # select topic
+    if st.session_state.add_topic:
+        chosen_topic = st.multiselect(
+            label='あなたがよく見るツイートのトピックを選択してください',
+            options=st.session_state.option_topic,
+            default=st.session_state.default_topic,
+        )
+    else:
+        chosen_topic = st.multiselect(
+            label='あなたがよく見るツイートのトピックを選択してください',
+            options=st.session_state.option_topic,
+            default=st.session_state.default_topic,
+        )
+        st.session_state.default_topic = chosen_topic
 
-    st.session_state.chosen_topic = []
-
-    st.multiselect(
-        label='あなたがよく見るツイートのトピックを選択してください',
-        options=option_topic,
-        list=st.session_state.chosen_topic
-    )
-
+    # add original topic
     topic = st.text_input(
         label='選択肢にないトピックを追加できます👇',
         value="",
         placeholder='(例)テニス、就活、...etc'
     )
+    st.text(topic)
+    if st.button(label='追加'):
+        if topic not in st.session_state.default_topic:
+            st.session_state.default_topic.append(topic)
+            if topic not in st.session_state.option_topic:
+                st.session_state.option_topic.append(topic)
+        st.session_state.add_topic = True
+        st.experimental_rerun()
+    
+    st.session_state.add_topic = False
 
+    next_btn()
+
+
+def next_btn():
     st.button(
-        label='追加',
-        on_click=add_topic(topic=topic)
+        "いいねしたツイートを見る！",
+        on_click=go_tweet_list,
+        # args=(chosen_topic,)
     )
 
-    st.button(
-        label="次のページへ",
-        # on_click=go_next_page()
-    )
-
-    def add_topic(topic):
-        st.session_state.chosen_topic.append(topic)
+def go_tweet_list():
+    st.session_state.page_name = 'tweet_list'
