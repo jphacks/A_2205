e935bb5f8ff57887270c79e27b7c5c1ae0d55ba5
YusukeTakahashi <yusuke@YusukeTakahashinoMacBook-Pro.local>
2022-10-18 15:59:53 +0900

    Added bg_set func

---
+++ frontend/utils.py
@@ -0,0 +1,22 @@
+import base64
+import streamlit as st
+
+def set_png_as_page_bg(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+ 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
+    page_bg_img = f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
