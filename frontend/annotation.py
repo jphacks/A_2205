ea03e5c99d771c759399962e1080ddf780d22f1c
Hiroto Kurita <73727292+kuriyan1204@users.noreply.github.com>
2022-10-21 21:25:17 +0900

    🤗 updated model

--- frontend/annotation.py
+++ frontend/annotation.py
@@ -2,29 +2,37 @@ import json
 import requests
 import streamlit as st
 
+
 def tweet_to_html(url):
     api = f"https://publish.twitter.com/oembed?url={url}"
     res = requests.get(api)
     response = res.json()
     return response["html"]
 
+
 def annotation():
     NUM_MIN_TWEET = 3
-    NUM_DEAFULT_SHOW_TWEET = 10
+    NUM_DEAFULT_SHOW_TWEET = 30
     # get tweet at only first time
     if not st.session_state.init_annotation:
-        res = requests.get(f"http://api_server:8080/tweets/{st.session_state.twitter_id}")
+        res = requests.get(
+            f"http://api_server:8080/tweets/{st.session_state.twitter_id}"
+        )
         st.session_state.not_annotated_tweets = json.loads(res.json()["data"])
         st.session_state.init_annotation = True
-    
+
     show_tweets = st.session_state.not_annotated_tweets[:NUM_DEAFULT_SHOW_TWEET]
 
     # count annotated topic
-    if st.session_state.annotate_count <= len(st.session_state.chosen_topic)-1:
-        if st.button('次のラベルへ'):
+    if st.session_state.annotate_count <= len(st.session_state.chosen_topic) - 1:
+        if st.button("次のラベルへ"):
             st.session_state.annotate_count += 1
             # exclude labeled tweets
-            st.session_state.not_annotated_tweets = [tw for tw in st.session_state.not_annotated_tweets if tw['id'] not in st.session_state.labels]
+            st.session_state.not_annotated_tweets = [
+                tw
+                for tw in st.session_state.not_annotated_tweets
+                if tw["id"] not in st.session_state.labels
+            ]
             show_tweets = st.session_state.not_annotated_tweets[:NUM_DEAFULT_SHOW_TWEET]
         # reload to hide this button
         if st.session_state.annotate_count == len(st.session_state.chosen_topic):
@@ -38,19 +46,19 @@ def annotation():
         topic = st.session_state.chosen_topic[st.session_state.annotate_count]
     except IndexError:
         topic = st.session_state.chosen_topic[-1]
-    num_selected_tweets = sum(True for v in st.session_state.labels.values() if v==topic)
+    num_selected_tweets = sum(
+        True for v in st.session_state.labels.values() if v == topic
+    )
     # topic progress bar
-    st.progress(st.session_state.annotate_count/len(st.session_state.chosen_topic))
+    st.progress(st.session_state.annotate_count / len(st.session_state.chosen_topic))
 
     if st.session_state.annotate_count != len(st.session_state.chosen_topic):
         # show title
-        st.header(
-            f'{topic}のツイートを選択してください'
-        )
+        st.header(f"{topic}のツイートを選択してください")
         # tweet selection progress bar
-        st.text(f'{num_selected_tweets}/{NUM_MIN_TWEET}')
-        st.progress(num_selected_tweets/NUM_MIN_TWEET)
-    
+        st.text(f"{num_selected_tweets}/{NUM_MIN_TWEET}")
+        st.progress(num_selected_tweets / NUM_MIN_TWEET)
+
         # show tweets
         for tweet in show_tweets:
             tweet_id = tweet["id"]
@@ -59,7 +67,7 @@ def annotation():
             html = tweet_to_html(url)
             with st.container():
                 check = st.checkbox(
-                    f'{topic}のツイートです',
+                    f"{topic}のツイートです",
                     key=tweet_id,
                 )
                 if check:
@@ -68,12 +76,12 @@ def annotation():
                     if not st.session_state.done_reload.get(tweet_id):
                         st.session_state.done_reload[tweet_id] = True
                         st.experimental_rerun()
-                st.components.v1.html(html, height=300, scrolling=True)
-                st.write('---')
+                st.markdown(html, unsafe_allow_html=True)
+                # st.components.v1.html(html, height=300, scrolling=True)
+                st.write("---")
     else:
-        st.header(
-            f'全てのアノテーションが完了しました🎉'
-        )
+        st.header(f"全てのアノテーションが完了しました🎉")
+
 
 def next_btn():
     st.button(
@@ -81,12 +89,13 @@ def next_btn():
         on_click=go_tweet_list,
     )
 
+
 def go_tweet_list():
     payload = {
-        'labels': list(st.session_state.labels.items()),
+        "labels": list(st.session_state.labels.items()),
     }
     res = requests.post(
         f"http://api_server:8080/annotation/{st.session_state.twitter_id}",
         json=payload,
     )
-    st.session_state.page_name = 'tweet_list'
+    st.session_state.page_name = "tweet_list"
