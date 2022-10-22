29120c3638a95706c903d7b7276612e4a354a395
Sota Nishiyama <sotanishy@Sotas-MacBook-Pro.local>
2022-10-22 10:34:09 +0900

    add username

--- backend/crawl.py
+++ backend/crawl.py
@@ -6,9 +6,10 @@ bearer_token = os.environ["TWITTER_BEARER_TOKEN"]
 api = tweepy.Client(bearer_token=bearer_token)
 
 
-def crawl_tweets(username: str):
-    df_tweets = pd.read_csv(f"users/{username}/tweets.csv")
-    user = api.get_user(username=username).data
+def crawl_tweets(username: str, twitter_id: str):
+    path = f"users/{username}/{twitter_id}/tweets.csv"
+    df_tweets = pd.read_csv(path)
+    user = api.get_user(username=twitter_id).data
     user_id = user.get("id")
 
     # get 100 recently liked tweets and remove duplicates
@@ -31,4 +32,4 @@ def crawl_tweets(username: str):
         ]
     )
     df_tweets = pd.concat([df_tweets_new, df_tweets])
-    df_tweets.to_csv(f"users/{username}/tweets.csv", index=False)
+    df_tweets.to_csv(path, index=False)
